b926c34bd2fe3bcddaa20163757a82c6b580415d
Felipe Vicalvi D Olivieira <felipe.oliveira@rsinet.com.br>
2020-03-19 14:05:59 -0300

    corrigindo problemas no projeto

---
+++ ProjetoTDD/src/main/java/Hub/ProjetoTDD/App.java
@@ -0,0 +1,13 @@
+package Hub.ProjetoTDD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
