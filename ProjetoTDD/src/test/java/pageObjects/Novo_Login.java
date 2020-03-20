3802a7302596517b59765f88256cff47f44ce23a
Felipe Vicalvi D Olivieira <felipe.oliveira@rsinet.com.br>
2020-03-20 14:11:05 -0300

    script login realizado com sucesso

---
+++ ProjetoTDD/src/test/java/pageObjects/Novo_Login.java
@@ -0,0 +1,35 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Novo_Login {
+
+	private static WebElement element = null;
+
+	public static WebElement txt_Usuario(WebDriver driver) {
+
+		element = driver.findElement(By.name("username"));
+
+		return element;
+
+	}
+
+	public static WebElement txt_Senha(WebDriver driver) {
+
+		element = driver.findElement(By.name("password"));
+
+		return element;
+
+	}
+
+	public static WebElement btn_Logar(WebDriver driver) {
+
+		element = driver.findElement(By.id("sign_in_btnundefined"));
+
+		return element;
+
+	}
+
+}
--- ProjetoTDD/src/test/java/testCases/Realizar_Login.java
+++ ProjetoTDD/src/test/java/testCases/Realizar_Login.java
@@ -1,10 +1,39 @@
 package testCases;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import pageObjects.Novo_Login;
+import pageObjects.Pagina_Inicial;
+
 public class Realizar_Login {
 
+	private static WebDriver driver = null;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String driverExecutablePath = "C:\\drivers\\chromedriver.exe";
+
+		System.setProperty("webdriver.chrome.driver", driverExecutablePath);
+
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+		driver.get("https://www.advantageonlineshopping.com/#/");
+
+		Pagina_Inicial.Usuario(driver).click();
+		Novo_Login.txt_Usuario(driver).sendKeys("Usuario");
+		Novo_Login.txt_Senha(driver).sendKeys("Ab123456");
+		Novo_Login.btn_Logar(driver).click();
+	
+	
 	}
-
+	
+	
+	
 }
