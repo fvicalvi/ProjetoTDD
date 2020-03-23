c09f8a6a7567b9c545397c3a92c6d09b162aa192
Felipe Vicalvi D Olivieira <felipe.oliveira@rsinet.com.br>
2020-03-23 10:03:42 -0300

    script finalizado e funcionando, aplicando teste com junit

--- ProjetoTDD/src/test/java/pageObjects/Pagina_Inicial.java
+++ ProjetoTDD/src/test/java/pageObjects/Pagina_Inicial.java
@@ -5,45 +5,43 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class Pagina_Inicial {
-	
-private static WebElement element = null;
-	
+
+	private static WebElement element = null;
+
 	public static WebElement Usuario(WebDriver driver) {
-		
+
 		element = driver.findElement(By.id("menuUserLink"));
-		
+
 		return element;
-		
-		}
-	
-	
+
+	}
+
 	public static WebElement Cadastro(WebDriver driver) {
-		
+
 		element = driver.findElement(By.linkText("CREATE NEW ACCOUNT"));
-		
+
 		return element;
 	}
-	
-	
+
 	public static WebElement Buscar(WebDriver driver) {
-		
+
 		element = driver.findElement(By.id("menuSearch"));
-		
+
 		return element;
 	}
-	
-	
+
 	public static WebElement Banner(WebDriver driver) {
-		
+
 		element = driver.findElement(By.id("headphonesImg"));
-		
+
 		return element;
 	}
 
-	
-	
-	
-	
+	public static WebElement produtoBanner(WebDriver driver) {
+
+		element = driver.findElement(By.id("15"));
+
+		return element;
+	}
+
 }
-
-
